64774750007d879561afdfaa05ed85a702712b53
Gian Huachin <ghuachin@micasita.com.pe>
2022-03-25 13:53:37 -0500

    Initial commit

---
+++ test/aws-cdk-oidc-github.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkOidcGithub from '../lib/aws-cdk-oidc-github-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-oidc-github-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkOidcGithub.AwsCdkOidcGithubStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
