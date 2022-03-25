b2d3bf131f33ee7dbae8d760202c09f78ede4d47
Gian Huachin <ghuachin@micasita.com.pe>
2022-03-25 13:59:41 -0500

    feat: add iam oidc provider for github repositories

--- lib/aws-cdk-oidc-github-stack.ts
+++ lib/aws-cdk-oidc-github-stack.ts
@@ -1,16 +1,23 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class AwsCdkOidcGithubStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a IAM OIDCProvider for Github repositories
+    const githubOidcProvider = new iam.OpenIdConnectProvider(this, 'GithubOidcProvider', {
+      url: 'https://token.actions.githubusercontent.com',
+      clientIds: [ 'sts.amazonaws.com' ],
+      thumbprints: ['6938FD4D98BAB03FAADB97B34396831E3780AEA1'] // This thumbprint will change if the github certificates changes.
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkOidcGithubQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CfnOutput(this, 'github-oidc-provider-arn-output', {
+      value: githubOidcProvider.openIdConnectProviderArn,
+      exportName: 'GITHUB_OIDC_PROVIDER_ARN'
+    });
+
+
   }
 }
